804ff661c56f55c6c5679bd61acdc3b05610c4ec
avhan42 <avhan42@gmail.com>
2022-08-21 19:27:42 +0700

    Set Icon Website

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Administrator | Website Amik Global Kendari</title>
 
+    {{-- favicon --}}
+    <link rel="shortcut icon" href="{{ asset('storage/' . $setting->logo) }}">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
